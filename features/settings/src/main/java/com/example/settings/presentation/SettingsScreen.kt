ab87fcff02e0fa90d2944d216e8397067e3f9874
aleksandr <sanek.v.07@gmail.com>
2025-07-21 00:31:47 +0300

    1. Рефакторинг: вынесена функция для выбора основного цвета приложения

--- features/settings/src/main/java/com/example/settings/presentation/SettingsScreen.kt
+++ features/settings/src/main/java/com/example/settings/presentation/SettingsScreen.kt
@@ -1,38 +1,16 @@
 package com.example.settings.presentation
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.common.domain.entity.ListItemModelUI
 import com.example.common.presentation.list.ListItem
 import com.example.common.presentation.list.TypeListItem
 import com.example.common.store.AppTheme
-import com.example.common.store.AppTheme.toColor
-
 
 @Composable
 fun SettingsScreen(
@@ -49,7 +27,6 @@ fun SettingsScreen(
         "Язык",
         "О программе"
     )
-    val isDarkTheme by viewmodel.isDarkTheme.collectAsStateWithLifecycle()
     Column {
         ListItem(
             itemModelUI = ListItemModelUI(
@@ -64,7 +41,7 @@ fun SettingsScreen(
                 viewmodel.setTheme(isDark)
                 onChangeTheme.invoke(isDark)
             },
-            isDarkTheme = isDarkTheme
+            isDarkTheme = viewmodel.isDarkTheme
         )
         val showDialogChangePrimaryColor = remember { mutableStateOf(false) }
         if (showDialogChangePrimaryColor.value) ChangePrimaryColor(showDialogChangePrimaryColor) { variant ->
@@ -93,75 +70,8 @@ fun SettingsScreen(
                     info = null,
                     typeListItem = TypeListItem.ARROW
                 ),
-                modifier = modifier.height(56.dp),
-                onClickChangeTheme = { isDark ->
-                    viewmodel.setTheme(isDark)
-                    onChangeTheme.invoke(isDark)
-                },
-                isDarkTheme = isDarkTheme
+                modifier = modifier.height(56.dp)
             )
         }
     }
 }
-
-@Composable
-private fun ChangePrimaryColor(
-    dialogState: MutableState<Boolean>,
-    onChangeColor: (AppTheme.PrimaryColorVariant) -> Unit
-) {
-    AlertDialog(
-        onDismissRequest = { dialogState.value = false },
-        title = { Text("Выберите цвет", color = MaterialTheme.colorScheme.onBackground) },
-        text = {
-            Column {
-                AppTheme.PrimaryColorVariant.entries.forEach { variant ->
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                onChangeColor.invoke(variant)
-                                dialogState.value = false
-                            }
-                            .padding(16.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .size(24.dp)
-                                .clip(CircleShape)
-                                .background(variant.toColor())
-                        )
-                        Spacer(Modifier.width(16.dp))
-                        Text(variant.title)
-                    }
-                }
-            }
-        },
-        confirmButton = {}
-    )
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- features/settings/src/main/java/com/example/settings/presentation/SettingsViewmodel.kt
+++ features/settings/src/main/java/com/example/settings/presentation/SettingsViewmodel.kt
@@ -5,20 +5,15 @@ import androidx.core.content.edit
 import androidx.lifecycle.ViewModel
 import com.example.common.store.AppTheme
 import com.example.common.store.NamedStore
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 class SettingsViewmodel @Inject constructor(
     private val sharedPreferences: SharedPreferences
 ) : ViewModel() {
-    private var _isDarkTheme =
-        MutableStateFlow(sharedPreferences.getBoolean(NamedStore.DARK_THEME, false))
-    val isDarkTheme: StateFlow<Boolean> = _isDarkTheme.asStateFlow()
+    val isDarkTheme: Boolean
+        get() = sharedPreferences.getBoolean(NamedStore.DARK_THEME, false)
 
     fun setTheme(isDark: Boolean) {
-        _isDarkTheme.value = isDark
         sharedPreferences.edit { putBoolean(NamedStore.DARK_THEME, isDark) }
     }
 
