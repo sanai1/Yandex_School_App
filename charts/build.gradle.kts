2f0f526a17d9ec1452b98717123de6e1c785c5a4
aleksandr <sanek.v.07@gmail.com>
2025-07-23 12:06:33 +0300

    1. Добавлен модуль для графиков 2. Добавлен модуль для круговой диаграммы

---
+++ charts/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.charts"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    api(project(":charts:pie"))
+}
--- charts/pie/build.gradle.kts
+++ charts/pie/build.gradle.kts
@@ -2,12 +2,10 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-
-    alias(libs.plugins.ksp)
 }
 
 android {
-    namespace = "com.example.common"
+    namespace = "com.example.pie"
     compileSdk = 35
 
     defaultConfig {
@@ -34,22 +32,10 @@ android {
 }
 
 dependencies {
-    api(project(":network"))
-    api(project(":database"))
-    api(project(":charts"))
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-
-    implementation(libs.airbnb.android.lottie.compose)
-    implementation(libs.androidx.navigation.compose)
-
-    api(libs.google.dagger.core)
-    ksp(libs.google.dagger.compiler)
 }
